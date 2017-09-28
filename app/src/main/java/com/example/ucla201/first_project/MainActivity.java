a9f560cda401f161eb6686b4d5c949a8ecd677b8
cmoorad <cmoorad@gmail.com>
2017-09-28 17:47:18 -0400

    testing

--- app/src/main/java/com/example/ucla201/first_project/MainActivity.java
+++ app/src/main/java/com/example/ucla201/first_project/MainActivity.java
@@ -3,13 +3,93 @@ package com.example.ucla201.first_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.image_view);
+        if( bitmap != null)
+            imageView.setImageBitmap(bitmap);
     }
+
+    public void camButtonClicked(View v){
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the bitmap
+        Log.d("STATE", "onSaveState");
+        outState.putParcelable("IMG", bitmap);
+    }
+
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("STATE", "onRestoreState");
+        bitmap = savedInstanceState.getParcelable("IMG");
+        imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+
+
+    public void focusChange(View.OnFocusChangeListener f) {
+        Intent changeFocusIntent = new Intent();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

72ace3e216ef43aebc9d6f73ff35dd714fdf4307
cmoorad <cmoorad@gmail.com>
2017-09-28 19:18:57 -0400

    testing

--- app/src/main/java/com/example/ucla201/first_project/MainActivity.java
+++ app/src/main/java/com/example/ucla201/first_project/MainActivity.java
@@ -11,10 +11,13 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextSwitcher;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,19 +35,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        imageView = findViewById(R.id.image_view);
+        imageView = findViewById(R.id.image);
         if( bitmap != null)
             imageView.setImageBitmap(bitmap);
     }
 
     public void camButtonClicked(View v){
-
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -71,21 +73,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //CLEAR BUTTON
+    public void clearButtonClicked(View v){
+        EditText x = findViewById(R.id.editHandle);
+        String y = x.toString();
+        if (y != "") x.setText("", TextView.BufferType.EDITABLE);
+
+        EditText x2 = findViewById(R.id.editName);
+        String y2 = x2.toString();
+        if (y2 != "") x2.setText("", TextView.BufferType.EDITABLE);
+
+        EditText x3 = findViewById(R.id.editPassword);
+        String y3 = x3.toString();
+        if (y3 != "") x3.setText("", TextView.BufferType.EDITABLE);
+
+    }
 
 
+/*
     public void focusChange(View.OnFocusChangeListener f) {
         Intent changeFocusIntent = new Intent();
 
 
     }
-
-
-
-
-
-
-
-
+*/
 
 
 
